7c47e0213c316910f5a721c9315e2f259eddba0d
hellfire <hellfire@gmail.com>
2021-01-18 20:49:51 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var enterAge = prompt('Введите свой возраст');
+if(enterAge <= 0){
+    alert('Ты шо совсем ебобо?');
+}else if(enterAge <= 6 ){
+    alert('Да ты еще молокосос!');
+}else if(enterAge <= 12){
+    alert('Молодая кровь');
+}else if(enterAge <= 18){
+    alert('Школьник');
+}else if(enterAge <= 150){
+    alert('ТЫ ДЕД ПЕРДЕД ПОЗДРАВЛЯЮ!!!');
+}else if(enterAge >= 151){
+    alert('Ты что инопланетянин!? О_о');
+}else{
+    alert('ПИШИ ТОЛЬКО ЦИФРЫ ШАЙТАН ! ! !')
+}
